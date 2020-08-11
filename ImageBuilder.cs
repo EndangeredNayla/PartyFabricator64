5eab8c25a834a6d8e4b0a26df1201821545d71a7
Henry Silva <66459009+Kazucroco@users.noreply.github.com>
2020-08-10 21:47:29 -0400

    Revived and released

--- ImageBuilder.cs
+++ ImageBuilder.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Windows.Media.Imaging;
 
 namespace PartyFabricator64
 {
@@ -31,17 +32,17 @@ namespace PartyFabricator64
 
             if (mp == 1) //Decides whether or not to use the Mario Party 1 exclusive winning / losing portraits.
             {
-                first = Image.FromFile($@"A:/programming/PartyFabricator64/assets/mp{mp}/characters/{p1.getPlayer()}w.png");
-                second = Image.FromFile($@"A:/programming/PartyFabricator64/assets/mp{mp}/characters/{p2.getPlayer()}n.png");
-                third = Image.FromFile($@"A:/programming/PartyFabricator64/assets/mp{mp}/characters/{p3.getPlayer()}n.png");
-                fourth = Image.FromFile($@"A:/programming/PartyFabricator64/assets/mp{mp}/characters/{p4.getPlayer()}l.png");
+                first = Image.FromFile($@"assets/mp{mp}/characters/{p1.getPlayer()}w.png");
+                second = Image.FromFile($@"assets/mp{mp}/characters/{p2.getPlayer()}n.png");
+                third = Image.FromFile($@"assets/mp{mp}/characters/{p3.getPlayer()}n.png");
+                fourth = Image.FromFile($@"assets/mp{mp}/characters/{p4.getPlayer()}l.png");
             }
             else
             {
-                first = Image.FromFile($@"A:/programming/PartyFabricator64/assets/mp{mp}/characters/{p1.getPlayer()}.png");
-                second = Image.FromFile($@"A:/programming/PartyFabricator64/assets/mp{mp}/characters/{p2.getPlayer()}.png");
-                third = Image.FromFile($@"A:/programming/PartyFabricator64/assets/mp{mp}/characters/{p3.getPlayer()}.png");
-                fourth = Image.FromFile($@"A:/programming/PartyFabricator64/assets/mp{mp}/characters/{p4.getPlayer()}.png");
+                first = Image.FromFile($@"assets/mp{mp}/characters/{p1.getPlayer()}.png");
+                second = Image.FromFile($@"assets/mp{mp}/characters/{p2.getPlayer()}.png");
+                third = Image.FromFile($@"assets/mp{mp}/characters/{p3.getPlayer()}.png");
+                fourth = Image.FromFile($@"assets/mp{mp}/characters/{p4.getPlayer()}.png");
             }
 
             Image result = new Bitmap(319, 237); //Initializes final results image.
@@ -49,8 +50,8 @@ namespace PartyFabricator64
             //IMAGE COMPOSITING FOR MARIO PARTY 1 AND 2
             if (mp == 1 || mp == 2) // These games have similar enough leaderboards to combine them into 1 section.
             {
-                Image bg = Image.FromFile($@"A:/programming/PartyFabricator64/assets/mp{mp}/boards/{board}.png");
-                Image overlay = Image.FromFile($@"A:/programming/PartyFabricator64/assets/mp{mp}/overlay.png");
+                Image bg = Image.FromFile($@"assets/mp{mp}/boards/{board}.png");
+                Image overlay = Image.FromFile($@"assets/mp{mp}/overlay.png");
 
                 using (Graphics gr = Graphics.FromImage(result))
                 {
@@ -62,34 +63,78 @@ namespace PartyFabricator64
                     gr.DrawImage(third, 94, 133);
                     gr.DrawImage(fourth, 94, 173);
 
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getStarChar(0, p1.getStars())}.png"), 166, 65);
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getStarChar(1, p1.getStars())}.png"), 181, 65);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getStarChar(0, p1.getStars(), false)}.png"), 166, 65);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getStarChar(1, p1.getStars(), false)}.png"), 181, 65);
 
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getStarChar(0, p2.getStars())}.png"), 166, 105);
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getStarChar(1, p2.getStars())}.png"), 181, 105);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getStarChar(0, p2.getStars(), false)}.png"), 166, 105);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getStarChar(1, p2.getStars(), false)}.png"), 181, 105);
 
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getStarChar(0, p3.getStars())}.png"), 166, 145);
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getStarChar(1, p3.getStars())}.png"), 181, 145);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getStarChar(0, p3.getStars(), false)}.png"), 166, 145);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getStarChar(1, p3.getStars(), false)}.png"), 181, 145);
 
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getStarChar(0, p4.getStars())}.png"), 166, 185);
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getStarChar(1, p4.getStars())}.png"), 181, 185);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getStarChar(0, p4.getStars(), false)}.png"), 166, 185);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getStarChar(1, p4.getStars(), false)}.png"), 181, 185);
 
 
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getCoinChar(0, p1.getCoins())}.png"), getCoinPos(0, p1.getCoins()), 65);
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getCoinChar(1, p1.getCoins())}.png"), getCoinPos(1, p1.getCoins()), 65);
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getCoinChar(2, p1.getCoins())}.png"), getCoinPos(2, p1.getCoins()), 65);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(0, p1.getCoins(), false)}.png"), getCoinPos(0, p1.getCoins()), 65);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(1, p1.getCoins(), false)}.png"), getCoinPos(1, p1.getCoins()), 65);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(2, p1.getCoins(), false)}.png"), getCoinPos(2, p1.getCoins()), 65);
 
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getCoinChar(0, p2.getCoins())}.png"), getCoinPos(0, p2.getCoins()), 105);
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getCoinChar(1, p2.getCoins())}.png"), getCoinPos(1, p2.getCoins()), 105);
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getCoinChar(2, p2.getCoins())}.png"), getCoinPos(2, p2.getCoins()), 105);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(0, p2.getCoins(), false)}.png"), getCoinPos(0, p2.getCoins()), 105);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(1, p2.getCoins(), false)}.png"), getCoinPos(1, p2.getCoins()), 105);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(2, p2.getCoins(), false)}.png"), getCoinPos(2, p2.getCoins()), 105);
 
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getCoinChar(0, p3.getCoins())}.png"), getCoinPos(0, p3.getCoins()), 145);
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getCoinChar(1, p3.getCoins())}.png"), getCoinPos(1, p3.getCoins()), 145);
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getCoinChar(2, p3.getCoins())}.png"), getCoinPos(2, p3.getCoins()), 145);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(0, p3.getCoins(), false)}.png"), getCoinPos(0, p3.getCoins()), 145);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(1, p3.getCoins(), false)}.png"), getCoinPos(1, p3.getCoins()), 145);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(2, p3.getCoins(), false)}.png"), getCoinPos(2, p3.getCoins()), 145);
 
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getCoinChar(0, p4.getCoins())}.png"), getCoinPos(0, p4.getCoins()), 185);
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getCoinChar(1, p4.getCoins())}.png"), getCoinPos(1, p4.getCoins()), 185);
-                    gr.DrawImage(Image.FromFile($@"A:/programming/PartyFabricator64/assets/type/{getCoinChar(2, p4.getCoins())}.png"), getCoinPos(2, p4.getCoins()), 185);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(0, p4.getCoins(), false)}.png"), getCoinPos(0, p4.getCoins()), 185);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(1, p4.getCoins(), false)}.png"), getCoinPos(1, p4.getCoins()), 185);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(2, p4.getCoins(), false)}.png"), getCoinPos(2, p4.getCoins()), 185);
+                }
+            } else { //If the Mario Party game specified isn't 1 or 2...
+
+                Image bg = Image.FromFile($@"assets/mp{mp}/background.png");
+                Image overlay = Image.FromFile($@"assets/mp{mp}/boards/{board}.png");
+
+                using (Graphics gr = Graphics.FromImage(result))
+                {
+                    gr.DrawImage(bg, 0, 0, bg.Width, bg.Height);
+                    gr.DrawImage(overlay, 0, 0, overlay.Width, overlay.Height);
+
+                    gr.DrawImage(first, 95, 53);
+                    gr.DrawImage(second, 95, 93);
+                    gr.DrawImage(third, 95, 133);
+                    gr.DrawImage(fourth, 95, 173);
+
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getStarChar(0, p1.getStars(), true)}.png"), 171, 64);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getStarChar(1, p1.getStars(), true)}.png"), 187, 64);
+
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getStarChar(0, p2.getStars(), true)}.png"), 171, 104);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getStarChar(1, p2.getStars(), true)}.png"), 187, 104);
+
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getStarChar(0, p3.getStars(), true)}.png"), 171, 144);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getStarChar(1, p3.getStars(), true)}.png"), 187, 144);
+
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getStarChar(0, p4.getStars(), true)}.png"), 171, 184);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getStarChar(1, p4.getStars(), true)}.png"), 187, 184);
+
+
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(0, p1.getCoins(), true)}.png"), getCoinPos3(0, p1.getCoins()), 64);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(1, p1.getCoins(), true)}.png"), getCoinPos3(1, p1.getCoins()), 64);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(2, p1.getCoins(), true)}.png"), getCoinPos3(2, p1.getCoins()), 64);
+
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(0, p2.getCoins(), true)}.png"), getCoinPos3(0, p2.getCoins()), 104);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(1, p2.getCoins(), true)}.png"), getCoinPos3(1, p2.getCoins()), 104);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(2, p2.getCoins(), true)}.png"), getCoinPos3(2, p2.getCoins()), 104);
+
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(0, p3.getCoins(), true)}.png"), getCoinPos3(0, p3.getCoins()), 144);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(1, p3.getCoins(), true)}.png"), getCoinPos3(1, p3.getCoins()), 144);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(2, p3.getCoins(), true)}.png"), getCoinPos3(2, p3.getCoins()), 144);
+
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(0, p4.getCoins(), true)}.png"), getCoinPos3(0, p4.getCoins()), 184);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(1, p4.getCoins(), true)}.png"), getCoinPos3(1, p4.getCoins()), 184);
+                    gr.DrawImage(Image.FromFile($@"assets/type/{getCoinChar(2, p4.getCoins(), true)}.png"), getCoinPos3(2, p4.getCoins()), 184);
                 }
             }
             return result;
@@ -111,7 +156,19 @@ namespace PartyFabricator64
             if (pl == 1) return 257;
 
             if (amt < 10) return 265; //In cases the total amount is less than 10, the x-coordinate is slightly offset.
-            else return 273;
+            else return 272;
+        }
+
+        /*
+         * Does the same thing as getCoinPos() but is for MP3. I didn't account for the fact that it would work differently.
+         */
+        private static int getCoinPos3(int pl, int amt)
+        {
+            if (pl == 0) return 245;
+            if (pl == 1) return 261;
+
+            if (amt < 10) return 261;
+            else return 277;
         }
 
         /*
@@ -119,7 +176,7 @@ namespace PartyFabricator64
          * 'pl' => Place value, should be 0-2 and account for numbers 0-999.
          * 'amt' => Amount, should be the total number of coins the player has.
          */
-        private static string getCoinChar(int pl, int amt)
+        private static string getCoinChar(int pl, int amt, bool mp3)
         {
             string chara;
 
@@ -128,7 +185,10 @@ namespace PartyFabricator64
             if (pl == 0)
             {
                 if (Math.Truncate((Double)amt / 100) != 0) chara = Math.Truncate((Double)amt / 100).ToString();
-                else chara = "x";
+                else {
+                    if (mp3) chara = "xs";
+                    else chara = "x";
+                }
             }
             else if (pl == 1)
             {
@@ -146,7 +206,7 @@ namespace PartyFabricator64
          *  'pl' => Place value, should be 0-1 and account for numbers 0-99.
          *  'amt' => Amount, should be the total number of stars the player has.
          */
-        private static string getStarChar(int pl, int amt)
+        private static string getStarChar(int pl, int amt, bool mp3)
         {
             string chara;
 
@@ -155,7 +215,10 @@ namespace PartyFabricator64
             if (pl == 0)
             {
                 if (Math.Truncate((Double)amt / 10) != 0) chara = Math.Truncate((Double)amt / 10).ToString();
-                else chara = "x";
+                else {
+                    if (mp3) chara = "xs";
+                    else chara = "x";
+                }
             }
             else if (pl == 1)
             {
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,17 +20,18 @@ namespace PartyFabricator64
     public partial class MainWindow : Window
     {
         int mp = 1;
+        bool init = false;
         Player p1 = new Player(0);
         Player p2 = new Player(1);
         Player p3 = new Player(2);
         Player p4 = new Player(3);
 
+        //Constructor, sets all of the ticks and boxes to their default values
         public MainWindow()
         {
             InitializeComponent();
 
             radMP1.IsChecked = true;
-            radMP3.IsEnabled = false;
 
             cbxBoard.SelectedIndex = 0;
 
@@ -48,20 +49,35 @@ namespace PartyFabricator64
             txtCSecond.Text = "0";
             txtCThird.Text = "0";
             txtCFourth.Text = "0";
+
+            init = true;
         }
 
+
+        /*
+         * Run whenever a character combo box is changed. 
+         * - Will update all player variable values, images in the application, and the results image itself.
+         */
         private void updateChar(object sender, SelectionChangedEventArgs e)
         {
+            init = false;
             p1.setPlayer(cbxP1.SelectedIndex, mp == 3);
             p2.setPlayer(cbxP2.SelectedIndex, mp == 3);
             p3.setPlayer(cbxP3.SelectedIndex, mp == 3);
             p4.setPlayer(cbxP4.SelectedIndex, mp == 3);
-
-            ImageBuilder.saveFile(ImageBuilder.makeResult(mp, cbxBoard.SelectedIndex, p1, p2, p3, p4));
+            updateImages();
+            init = true;
+            if (init) ImageBuilder.saveFile(ImageBuilder.makeResult(mp, cbxBoard.SelectedIndex, p1, p2, p3, p4));           
         }
 
+
+        /*
+         * Runs whenever the game radio button at the top is changed to MP1.
+         * - Changes the list of available boards and adjusts character select accordingly.
+         */
         private void radMP1_Checked(object sender, RoutedEventArgs e)
         {
+            init = false;
             mp = 1;
 
             for (int i = cbxBoard.Items.Count; i > 0; i--) cbxBoard.Items.RemoveAt(i - 1);
@@ -110,11 +126,21 @@ namespace PartyFabricator64
                     cbxP4.Items.RemoveAt(i - 1);
                 }
             }
-            ImageBuilder.saveFile(ImageBuilder.makeResult(mp, cbxBoard.SelectedIndex, p1, p2, p3, p4));
+
+            init = true;
+            updateImages();
+
+            if (init) ImageBuilder.saveFile(ImageBuilder.makeResult(mp, cbxBoard.SelectedIndex, p1, p2, p3, p4));
         }
 
+
+        /*
+         * Runs whenever the game radio button at the top is changed to MP2.
+         * - Changes the list of available boards and adjusts character select accordingly.
+         */
         private void radMP2_Checked(object sender, RoutedEventArgs e)
         {
+            init = false;
             mp = 2;
 
             for (int i = cbxBoard.Items.Count; i > 0; i--) cbxBoard.Items.RemoveAt(i - 1);
@@ -161,11 +187,19 @@ namespace PartyFabricator64
                     cbxP4.Items.RemoveAt(i - 1);
                 }
             }
-            ImageBuilder.saveFile(ImageBuilder.makeResult(mp, cbxBoard.SelectedIndex, p1, p2, p3, p4));
+            init = true;
+            updateImages();
+            if (init) ImageBuilder.saveFile(ImageBuilder.makeResult(mp, cbxBoard.SelectedIndex, p1, p2, p3, p4));
         }
 
+
+        /*
+         * Runs whenever the game radio button at the top is changed to MP3.
+         * - Changes the list of available boards and adds the MP3-exclusive characters to the list.
+         */
         private void radMP3_Checked(object sender, RoutedEventArgs e)
         {
+            init = false;
             mp = 3;
 
             for (int i = cbxBoard.Items.Count; i > 0; i--) cbxBoard.Items.RemoveAt(i - 1);
@@ -175,6 +209,7 @@ namespace PartyFabricator64
             cbxBoard.Items.Add("Woody Woods");
             cbxBoard.Items.Add("Creepy Cavern");
             cbxBoard.Items.Add("Waluigi's Island");
+            cbxBoard.Items.Add("Custom Board");
             cbxBoard.SelectedIndex = 0;
 
             cbxP1.Items.Add("Daisy");
@@ -185,10 +220,18 @@ namespace PartyFabricator64
             cbxP3.Items.Add("Waluigi");
             cbxP4.Items.Add("Daisy");
             cbxP4.Items.Add("Waluigi");
+            init = true;
 
-            ImageBuilder.saveFile(ImageBuilder.makeResult(mp, cbxBoard.SelectedIndex, p1, p2, p3, p4));
+            updateImages();
+
+            if (init) ImageBuilder.saveFile(ImageBuilder.makeResult(mp, cbxBoard.SelectedIndex, p1, p2, p3, p4));
         }
 
+
+        /*
+         * Runs whenever any of the coin or star balance text boxes are updated.
+         * - Updates the player variables with proper coin and star count, saves the results image.
+         */
         private void balanceUpdate(object sender, TextChangedEventArgs e)
         {
             p1.setStars(parseStars(txtSFirst.Text));
@@ -201,9 +244,22 @@ namespace PartyFabricator64
             p3.setCoins(parseCoins(txtCThird.Text));
             p4.setCoins(parseCoins(txtCFourth.Text));
 
-            ImageBuilder.saveFile(ImageBuilder.makeResult(mp, cbxBoard.SelectedIndex, p1, p2, p3, p4));
+            if (init) ImageBuilder.saveFile(ImageBuilder.makeResult(mp, cbxBoard.SelectedIndex, p1, p2, p3, p4));
         }
 
+        /*
+         * Runs whenever the board is changed.
+         * - Updates results image.
+         */
+        private void cbxBoard_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (init) ImageBuilder.saveFile(ImageBuilder.makeResult(mp, cbxBoard.SelectedIndex, p1, p2, p3, p4));
+        }
+
+        /*
+         * Helper used to try to convert the value in the text boxes to a star count.
+         * If it cannot be done, the value will default to 0.
+         */
         private int parseStars(string str)
         {
             int numS;
@@ -215,6 +271,10 @@ namespace PartyFabricator64
 
         }
 
+        /*
+         * Helper used to try to convert the value in the text boxes to a coin count.
+         * If it cannot be done, the value will default to 0.
+         */
         private int parseCoins(string str)
         {
             int numC;
@@ -225,9 +285,28 @@ namespace PartyFabricator64
             return numC;
         }
 
-        private void cbxBoard_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        /*
+         * Updates the previews for the character portrait images in the application window. Doesn't affect the actual results image whatsoever.
+         */
+        private void updateImages()
         {
-            ImageBuilder.saveFile(ImageBuilder.makeResult(mp, cbxBoard.SelectedIndex, p1, p2, p3, p4));
+            if (init)
+            {
+                if (mp == 1)
+                {
+                    imgFirst.Source = new BitmapImage(new Uri($@"assets/mp{mp}/characters/{p1.getPlayer()}w.png", UriKind.Relative));
+                    imgSecond.Source = new BitmapImage(new Uri($@"assets/mp{mp}/characters/{p2.getPlayer()}n.png", UriKind.Relative));
+                    imgThird.Source = new BitmapImage(new Uri($@"assets/mp{mp}/characters/{p3.getPlayer()}n.png", UriKind.Relative));
+                    imgFourth.Source = new BitmapImage(new Uri($@"assets/mp{mp}/characters/{p4.getPlayer()}l.png", UriKind.Relative));
+                }
+                else
+                {
+                    imgFirst.Source = new BitmapImage(new Uri($@"assets/mp{mp}/characters/{p1.getPlayer()}.png", UriKind.Relative));
+                    imgSecond.Source = new BitmapImage(new Uri($@"assets/mp{mp}/characters/{p2.getPlayer()}.png", UriKind.Relative));
+                    imgThird.Source = new BitmapImage(new Uri($@"assets/mp{mp}/characters/{p3.getPlayer()}.png", UriKind.Relative));
+                    imgFourth.Source = new BitmapImage(new Uri($@"assets/mp{mp}/characters/{p4.getPlayer()}.png", UriKind.Relative));
+                }
+            }
         }
     }
 }
